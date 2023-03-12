8a51f7479b146eb9160adfaedb40df1a963f366d
Abed.Matini <abed.matini@ogilvy.co.za>
2023-03-12 14:12:13 +0200

    dashboard removed

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -7,17 +7,21 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @foreach($notes as $note)
+            @forelse($notes as $note)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <h2 class="font-bold text-2x1">
                         {{ $note->title }}
                     </h2>
                     <p class="mt-2">
-                        {{ $note->text }}
+                        {{ Str::limit($note->text, 10) }}
                     </p>
-                    <span>{{ $note->updated_at }}</span>
+                    <span class="block mt-4 text-sm opacity-70">{{ $note->updated_at->diffForHumans() }}</span>
                 </div>
-            @endforeach
+            @empty
+                <p>You have no notes yes.</p>
+            @endforelse
+
+            {{ $notes->links() }}
         </div>
     </div>
 </x-app-layout>
